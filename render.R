4c949cb3f014e7703fdb5ded69bafe4a51abeddd
Michael Scherzer <u1003326@hci.utah.edu>
2023-10-17 08:16:22 -0600

    test

---
+++ render.R
@@ -0,0 +1,4 @@
+library(quarto)
+Sys.setenv(RSTUDIO_PANDOC = 'C:/Program Files/RStudio/resources/app/bin/quarto/bin/tools')
+quarto::quarto_render(input = "C:/Users/u1003326/OneDrive - University of Utah/Desktop/git/nevus/index.qmd",output_format = "html") 
+
